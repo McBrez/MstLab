a780668ac0d4e723aeb719db33e22c7d538b018d
Freismuth David <e1326907@student.tuwien.ac.at>
2019-12-13 09:41:48 +0100

    - Changed config file to JSON format. - Unfinished changes in SentinelConfig.py

--- Sentinel/SentinelConfig.py
+++ Sentinel/SentinelConfig.py
@@ -1,71 +1,95 @@
 """
-This program has been created as part of the MST lab lecture of  the institute of micromechanics 
-TU Wien.
-This script encapsulates the configuration of the sentinel and provides functions for reading in 
-an XML file, containing inital configuration.
+This program has been created as part of the MST lab lecture of the institute
+of micromechanics TU Wien.
+This script encapsulates the configuration of the sentinel and provides 
+functions for reading in an JSON file, containing inital configuration.
 
 Author: David FREISMUTH
 Date: DEC 2019
 License: 
 """
 
-import xml.etree.ElementTree as ET
+import json
 import os
 
 class SentinelConfig:
+    """
+    Encapsulates configuration data. Exposes functions for reading in a
+    configurations file and retrieving configuration data.
+    """
 
-    # The XML tag, that specifies the database name.
-    XML_DATABASE_NAME = "./DatabaseConfig/DatabaseName"
+    # Containts configuration keys and their representation in the JSON 
+    # configuration file. At __init__() of this object, the keys and their
+    # valus will be written as instance attributes to this object. 
+    __CONFIG_KEY_DICT = {
+        # Points to the root element of the Database configuration. Will return
+        # a dictionary containing the databse configurations.
+        "JSON_DATABASE_CONFIG" : "DatabaseConfig",
 
-    # The XML tag, that specifies the databse buffer size.
-    XML_DATABASE_BUFFER_SIZE = "./DatabaseConfig/AquisitionBufferSize"
+        # The name of the database file, that will be created/connected to on
+        # startup.
+        "JSON_DATABASE_NAME" : "DatabaseName",
 
-    # The XML tag, that specifies database storageIntervall
-    XML_DATABASE_STORAGE_INTERVALL = "./DatabaseConfig/DatabaseWriteIntervall"
-    
-    # The XML tag, that specifies the Xpath to MeasurementConfig tag.
-    XML_MEASUREMENT_CONFIG = "./MeasurementConfig"
+        # The size of the acquisition buffer.
+        "JSON_ACQUISITION_BUFFER" : "AcquisitionBufferSize",
 
-    # The XML tag, that specifies the Xpath to Measurement tag.
-    XML_MEASUREMENT = "Measurement"
+        # How often the value cache is written back to database. Value is in 
+        # milli seconds.
+        "JSON_WRITE_INTERVALL" : "WriteIntervall",
 
-    # The XML tag, that specifies the Xpath to Measurement tag.
-    XML_MEASUREMENT_SCAN_RATE = "ScanRate"
+        # Points to the Measurement configurations. Will return a list of
+        # dictionraries, containing measurement configurations.
+        "JSON_MEASUREMENT_CONFIG" : "MeasurmentConfig",
 
-    XML_MEASUREMENT_CONFIG_TAG = "ConfigTag"
+        # The name of the measurment configuration.
+        "JSON_MEASUREMENT_NAME" : "ConfigName",
 
-    XML_MEASUREMENT_CHANNEL = "Channels/Channel"
+        # The channels of the DAQ hardware, that shall be used. returns a 
+        # dictionary, that maps from channel number to channel tag.
+        "JSON_MEASUREMENT_CHANNELS" : "Channels",
 
-    XML_MEASUREMENT_CHANNEL_TAG = "Tag"
+        # The rate with which the values shall be sampled.
+        "JSON_MEASUREMENT_SCANRATE" : "ScanRate",
 
-    XML_MEASUREMENT_CHANNEL_PORT = "Port"
-
-    # The XLM tag, that specifies the channel tag.
-    XML_MEASUREMENT_TAG = "./MeasurementConfig/Measurement/Channel/" 
-
+        # Contains the measurements that shall be done. The results of these
+        # measurements will be written to database. Returns a dictionary where
+        # the keys are user defined names of the measurments. The values are 
+        # mathematical expressions, that state how the value of the measuremnt 
+        # shall be calculated from the values that have been read from the 
+        # DAQ card. The mathematical expressions have to be given as a string,
+        # that is parseable by the praser.eval() python function. The Variables 
+        # in this expression are the channel names, defined via the 
+        # JSON_MEASUREMENT_CHANNELS configuration.
+        "JSON_MEASUREMENTS" : "Measurements" 
+    }
+   
     def __init__(self, configFileName):
         """
-        Reads in the XML file given with configFileName, and constructs the SentinelConfig object
-        accordingly.
+        Reads in the JSON file given with configFileName, and constructs the 
+        SentinelConfig object accordingly.
 
         Parameters:
-        configFileName (string): Path to the XML file, containing initial config for the sentinel.
+        configFileName (string): Path to the JSON file, containing initial 
+        config for the sentinel.
         """
 
         # Check if file exists.
         if(not os.path.isfile(configFileName)):
             self.__valid = False
             return
-        
-        # Read in the configuration tree from the XML file.
-        tree = ET.parse(configFileName)
 
-        # Write it to object, if read was successfull
-        if(not tree):
+        # Parse __CONFIG_KEY_DICT into instance attributes.
+        for key, value in __CONFIG_KEY_DICT.items():
+            self.key = value
+
+        # Read the JSON file into a dict.
+        jsonFilePtr = open(configFileName, 'r')
+        self.configDict = json.load(jsonFilePtr)
+
+        if self.configDict != None:
+            self.__valid = True
+        else:
             self.__valid = False
-        
-        self.__configTree = tree.getroot()
-        self.__valid = True
 
     def isValid(self):
         """
@@ -77,15 +101,51 @@ class SentinelConfig:
 
         return self.__valid
 
-    def getConfigTree(self):
+    def getConfig(self, configKey):
         """
-        Returns the configuration tree, parsed by this object, provided this object is valid.
+        Returns configuration data, specified by configKey. configKey can be 
+        one of the JSON_* constants of SentinelConfig.
+
+        Parameters:
+        configKey (string): Specifies what configuration data should be 
+        retrieved. Can be one of the JSON_* constants of SentinelConfig.
 
         Returns:
-        An configuration tree, if this object is valid. None otherwise.
+        The configuration data specified by configKey. None if the configuration
+        could not be found.
+
+        Throws:
+        Exception: When the requested configuration data should be present, but
+        was missing in the JSON config file.
         """
 
-        if(self.__valid):
-            return self.__configTree
+        # Check if configKey is a known configuration key.
+        configFound = False
+
+        if configKey == SentinelConfig.JSON_DATABASE_CONFIG:
+            if SentinelConfig.JSON_DATABASE_CONFIG in self.__configDict.keys():
+                retVal = self.__configDict[configKey]
+                configFound = True
+
+        elif configKey == SentinelConfig.JSON_DATABASE_NAME:
+            if SentinelConfig.JSON_DATABASE_NAME in self.__configDict.keys():
+                retVal = self.__configDict[configKey]
+                configFound = True      
+
+        elif configKey == SentinelConfig.JSON_ACQUISITION_BUFFER:
+
+        elif configKey == SentinelConfig.JSON_WRITE_INTERVALL:
+
+        elif configKey == SentinelConfig.JSON_MEASUREMENT_CONFIG:
+
+        elif configKey == SentinelConfig.JSON_MEASUREMENT_NAME:
+
+        elif configKey == SentinelConfig.JSON_MEASUREMENT_CHANNELS:
+
+        elif configKey == SentinelConfig.JSON_MEASUREMENT_SCANRATE:
+        
+        elif configKey == SentinelConfig.JSON_MEASUREMENTS:
+
         else:
-            return None
+            # Unknown configuration key.
+            retVal = None
