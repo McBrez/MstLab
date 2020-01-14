1f78a05eae9dff0bcf9bf6b99249b8d7ce276412
Freismuth David <e1326907@student.tuwien.ac.at>
2020-01-14 07:03:54 +0100

    Modified DataAquisition so that is uses continuous scan instead of single shot scan.

--- Sentinel/DataAquisition.py
+++ Sentinel/DataAquisition.py
@@ -21,7 +21,7 @@ from datetime import datetime
 import parser
 
 # MC118 imports
-from daqhats import mcc118, OptionFlags, HatIDs, HatError
+from daqhats import mcc118, OptionFlags, HatIDs, HatError, namedtuple
 from daqhats_utils import select_hat_device, enum_mask_to_string,\
      chan_list_to_mask
 
@@ -38,9 +38,13 @@ class DataAquisition:
         aquisition.
     """
 
+    # Contant that specifies, that all available data shall be read.
     READ_ALL_AVAILABLE = -1
     CURSOR_BACK_2 = '\x1b[2D'
     ERASE_TO_END_OF_LINE = '\x1b[0K'
+    
+    # Time the scan buffer is popped. In seconds.
+    __SCAN_SLEEP_TIME = 0.8
 
     def __init__(self, configObject, storeFunc):
         """
@@ -48,7 +52,7 @@ class DataAquisition:
         DataAquisition object and registers the storage function that is used
         to pass the measured valus to database interface.
 
-        parameters:
+        Parameters:
             configObject (DataAquisitionConfig): Contains the configuration for 
             creation of this object.
             storeFunc ((void)(storeObj)): A refernce to function, that is called
@@ -56,11 +60,17 @@ class DataAquisition:
             and take a storeObj. 
         """
 
-        # Register worker function as Thread
+        # Register worker function as Thread.
         self.__workerThread = threading.Thread(
             group = None,
-            target = self.__workerFunction,
+            target = self.__scanningFunction,
             name = "AcquisitionThread")
+
+        # Register storage function as Thread.
+        self.__storageThread = threading.Thread(
+            group = None,
+            target = self.__storageFunction,
+            name = "StorageThread")
 
         # Load configuration objects.
         self.__configObject = configObject
@@ -80,9 +90,30 @@ class DataAquisition:
         # The active measurement configuration index.
         self.__activeMeasConfigIdx = 0
 
+        # Stores active measurement configuration.
+        self.__activeMeasConfig = []
+            
+        # Stores current active set of calucaltions.
+        self._currCalculations = {}
+
+        # Stores the current active measurement configuraiton name.
+        self._currMeasurementConfigName = ""
+
         # A semaphore is needed on confugration change, so only one one call 
         # to self.changeMeasConfig() at a time is possible
         self.__changeMeasConfSem = threading.Semaphore(value = 1)
+
+        # Storage variable for acquired data.
+        self.__acquiredData = namedtuple()
+    
+        # Current scan rate.
+        self.__currScanRate = 0.0
+
+        # Timestamp of the last set of acquired data.
+        self.__currTimestamp = datetime.now()
+
+        # Dictionary that maps Channel number to tag name.
+        self.__currChannelDict = {}
 
     def start(self):
         """
@@ -92,65 +123,120 @@ class DataAquisition:
         self.__runThread = True
         self.__workerThread.start()
 
-    def __workerFunction(self):
+    def __scanningFunction(self):
         """
-        Worker function, that can be called as thread. Initializes measurement
-        config and contains measurment loop.  
+        Worker function, that is called as thread. Initializes continuous 
+        measurement, and triggers data processing and storage functions.
         """
-
-        # Get constant for single shot read.
-        options = OptionFlags.DEFAULT
 
         # Get current measurement configuration and some values out of it.
-        activeMeasConfig = self.__measurementConfig[self.__activeMeasConfigIdx]
-        readIntervall = \
-            activeMeasConfig[SentinelConfig.JSON_MEASUREMENT_SCANRATE]
-        calculations = \
-            activeMeasConfig[SentinelConfig.JSON_MEASUREMENTS]
-        measurementConfigName = \
-            activeMeasConfig[SentinelConfig.JSON_MEASUREMENT_NAME]
-
-        # Convert from milli seconds to seconds.
-        readIntervall = readIntervall / 1000.0
+        self.__activeMeasConfig = \
+            self.__measurementConfig[self.__activeMeasConfigIdx]
+        self.__currScanRate = \
+            self.__activeMeasConfig[SentinelConfig.JSON_MEASUREMENT_SCANRATE]
+        self.__currCalculations = \
+            self.__activeMeasConfig[SentinelConfig.JSON_MEASUREMENTS]
+        self.__currMeasurementConfigName = \
+            self.__activeMeasConfig[SentinelConfig.JSON_MEASUREMENT_NAME]
 
         # Write configured channel numbers from configuration into 
-        # channelNums
+        # channelNums and create a mask from it.
         channelNums = []
-        channelDict = \
-            activeMeasConfig[SentinelConfig.JSON_MEASUREMENT_CHANNELS]
+        self.__CurrChannelDict = \
+            self.__activeMeasConfig[SentinelConfig.JSON_MEASUREMENT_CHANNELS]
         for channel in channelDict.keys():
             channelNums.append(int(channel))
+        channel_mask = chan_list_to_mask(channelNums)
 
         # Get an instance of the selected hat device object.
         address = select_hat_device(HatIDs.MCC_118)
         hat = mcc118(address)
 
-        # Measurement Loop.
-        channelValues = {}
+        # Cleanup scanning ressources.
+        hat.a_in_scan_cleanup()
+
+        # Trigger scanning.
+        hat.a_in_scan_start(
+            channel_mask  = channel_mask,
+            scan_rate = self.__currScanRate,
+            options = OptionFlags.CONTINUOUS)
+
+        # Measurement loop.
         while self.__runThread:
+            # MCC118 library automatically creates a buffer for at least a 
+            # second worth of samples. To leave some margin for error, the 
+            # buffer is popped every 0.8 seconds.
+            sleep(DataAquisition.__SCAN_SLEEP_TIME)
 
-            # Construct timestamp.
-            timestamp = datetime.now().isoformat()
-         
-            # Read a single value from each configured channel.
-            channelValues.clear()
-            for chan in channelNums:
-                channelTag = channelDict[str(chan)]
-                channelValues[channelTag] = hat.a_in_read(chan, options)
+            # Read all available samples from all channels. Timeout is ignored.
+            self.__acquiredData = hat.a_in_scan_read(
+                samples_per_channel = DataAquisition.READ_ALL_AVAILABLE,
+                timeout = 0)
 
-            # Execute configured measurement calculations.
-            for name, expr in calculations.items():
+            # Get current timestamp.
+            self.__currTimestamp = datetime.now()
+
+            # Redefine storage function thread object and start it.
+            self.__storageThread = threading.Thread(
+                group = None,
+                target = self.__storageFunction,
+                name = "StorageThread")     
+            self.__storageThread.start()
+
+        # Stop scanning.
+        hat.a_in_scan_start()
+
+    def __storageFunction(self):
+        """
+        Worker function, that is called by __scanningFunction() as a thread, to
+        trigger data processing and storage of acquired data. Data and 
+        additional information is taken from the __acquiredData, 
+        __currScanRate and __currTimestamp instance variables. The timestamps of
+        the single acquired valus is reproduced by taking __currTimestamp and
+        tracing back 1/__currScanRate seconds for each acquired value.
+        """
+
+        # Check for an overrun error
+        if self.__acquiredData.hardware_overrun:
+            print('\n\nHardware overrun\n')
+            return
+        elif self.__acquiredData.buffer_overrun:
+            print('\n\nBuffer overrun\n')
+            return
+
+        # Pop from acquired data, until it is empty.
+        loopCount = 0
+        while len(self.__acquiredData) != 0:
+            # Pop once for every configured channel. More recent values have 
+            # higher indices. Values from different channels are ordered 
+            # sequentially in the list. I.e for 4 channels -> 
+            # [1,2,3,4,1,2,3,4,...]
+            # The Values that are popped during one while loop iteration are 
+            # considered as concurrent.
+            channelValues = {}
+            for chanNum, chanTag in self.__currChannelDict.items().reverse():
+                channelValues[chanTag] = self.__acquiredData.pop()
+
+            # Calculate timestamp for current channelVAlues by starting from the
+            # original timestamp, and then subtracting the sample intervall 
+            # multiplied by the loop iteration count.
+            offset = (1.0 / self.__currScanRate) * loopCount
+            timestampDelta = datetime.timedelta(usec = offset)
+            reproducedTimestamp = self.__currTimestamp - timestampDelta
+
+            # Execute configured measurement calculations with the set of 
+            for name, expr in self.__currCalculations.items():
                 # Evaluate expression
-                value = self.__doCalculation(expr, channelDict, channelValues)
-                
+                value = self.__doCalculation(
+                    expr = expr,
+                    channelDict = self.__currChannelDict,
+                    channelValues = channelValues)
+
                 # Hand calculated value over to database interface.
                 measurementName = \
-                    measurementConfigName + "_" + name
-                valueCache = dict([(timestamp,value)])
+                    self.__currMeasurementConfigName + "_" + name
+                valueCache = dict([(timestamp-timestampDelta,value)])
                 self.__storeFunc(measurementName, valueCache)
-
-            # Wait the configured interval between reads.
-            sleep(readIntervall)
 
     def changeMeasConfig(self, measConfIdx):
         """
@@ -209,7 +295,7 @@ class DataAquisition:
 
     def __doCalculation(self, expr, channelDict, channelValues):
         """
-        Replaces the channel tags in the mathematial expression expr with 
+        Replaces the channel tags in the mathematical expression expr with 
         measurement values, given in channelValues.
 
         Parameters:
@@ -236,5 +322,3 @@ class DataAquisition:
         
         # Evaluate expression
         return eval(expr)
-
-        
